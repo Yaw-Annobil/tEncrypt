6be68ba442a3fac5bc6cf3faba30b97d7c4ab21c
Johnny <johnametepeagboku@live.com>
2021-08-25 17:17:17 +0000

    Built the Main Navigation

--- app/src/main/java/com/steg/tencrypt/MainActivity.java
+++ app/src/main/java/com/steg/tencrypt/MainActivity.java
@@ -3,10 +3,15 @@ package com.steg.tencrypt;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.steg.tencrypt.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     ActivityMainBinding binding;
 
     @Override
@@ -15,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        NavController nav =
+                ((NavHostFragment)getSupportFragmentManager()
+                        .findFragmentById(R.id.nav_host))
+                        .getNavController();
 
+        AppBarConfiguration configuration =
+                new AppBarConfiguration.Builder(nav.getGraph()).build();
+
+        NavigationUI.setupWithNavController(null, nav,configuration);
     }
 }
--- app/src/main/java/com/steg/tencrypt/ui/MytCryptsFragment.java
+++ app/src/main/java/com/steg/tencrypt/ui/MytCryptsFragment.java
@@ -2,6 +2,5 @@ package com.steg.tencrypt.ui;
 
 import androidx.fragment.app.Fragment;
 
-public class SetFragment extends Fragment {
-
+public class MytCryptsFragment extends Fragment {
 }

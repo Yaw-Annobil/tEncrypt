42c8369955615e82195809da8304905788656fa8
Johnny <johnametepeagboku@live.com>
2021-08-27 20:03:26 +0000

    Fixed Error in MaskedCard

--- app/src/main/java/com/steg/tencrypt/utilities/MaskedCardView.kt
+++ app/src/main/java/com/steg/tencrypt/utilities/MaskedCardView.kt
@@ -1,34 +1,51 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.steg.tencrypt.utilities
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
 import com.google.android.material.R
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.shape.ShapeAppearancePathProvider
-import android.annotation.SuppressLint
-import android.graphics.Path
-import android.graphics.RectF
 
 /**
  * A Card view that clips the content of any shape, this should be done upstream in card,
  * working around it for now.
  */
 class MaskedCardView @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyle: Int = R.attr.materialCardViewStyle
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = R.attr.materialCardViewStyle
 ) : MaterialCardView(context, attrs, defStyle) {
     @SuppressLint("RestrictedApi")
     private val pathProvider = ShapeAppearancePathProvider()
     private val path: Path = Path()
-    private val shapeAppearance: ShapeAppearanceModel = ShapeAppearanceModel(
-            context,
-            attrs,
-            defStyle,
-            R.style.Widget_MaterialComponents_CardView
-    )
+    private val shapeAppearance: ShapeAppearanceModel = ShapeAppearanceModel.builder(
+        context,
+        attrs,
+        defStyle,
+        R.style.Widget_MaterialComponents_CardView
+    ).build()
+
     private val rectF = RectF(0f, 0f, 0f, 0f)
 
     override fun onDraw(canvas: Canvas) {
@@ -44,4 +61,3 @@ class MaskedCardView @JvmOverloads constructor(
         super.onSizeChanged(w, h, oldw, oldh)
     }
 }
-

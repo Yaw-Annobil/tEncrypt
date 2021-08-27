39a883a1fc02b4f3f3ee45bb994ed54aa4e971fa
Johnny <johnametepeagboku@live.com>
2021-08-27 19:56:31 +0000

    Initialized the click listener on home cards

--- app/src/main/java/com/steg/tencrypt/ui/HomeFragment.java
+++ app/src/main/java/com/steg/tencrypt/ui/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.steg.tencrypt.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,9 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.steg.tencrypt.R;
 import com.steg.tencrypt.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding binding;
@@ -28,8 +32,21 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        List<View> views = new ArrayList<>();
+        
+        views.add(binding.encrypt);
+        views.add(binding.decrypt);
+        views.add(binding.history);
+        views.add(binding.template);
+        views.add(binding.set);
+
+        for (View i : views) {
+            i.setOnClickListener(this::navTo);
+        }
+
     }
 
+    @SuppressLint("NonConstantResourceId")
     void navTo(View view){
         int id = view.getId();
 
@@ -37,6 +54,21 @@ public class HomeFragment extends Fragment {
             case R.id.encrypt:
                 NavHostFragment.findNavController(this).
                         navigate(HomeFragmentDirections.encrypt());
+            case R.id.decrypt:
+                NavHostFragment.findNavController(this).
+                        navigate(HomeFragmentDirections.decrypt(""));
+            case R.id.history:
+                NavHostFragment.findNavController(this).
+                        navigate(HomeFragmentDirections.history());
+            case R.id.template:
+                NavHostFragment.findNavController(this).
+                        navigate(HomeFragmentDirections.template());
+            case R.id.set:
+                NavHostFragment.findNavController(this).
+                        navigate(HomeFragmentDirections.set());
+            default:
+                throw new IllegalArgumentException();
+                
         }
     }
 }

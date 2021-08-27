4cfb3f036391e68222e779b13b70306f9877a935
Yaw Annobil <84484595+Yaw-Annobil@users.noreply.github.com>
2021-08-27 19:33:23 +0000

    initialized UIs

--- app/src/main/java/com/steg/tencrypt/ui/FullScreenFragment.java
+++ app/src/main/java/com/steg/tencrypt/ui/FullScreenFragment.java
@@ -1,6 +1,29 @@
 package com.steg.tencrypt.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.steg.tencrypt.databinding.FragmentFullscreenBinding;
+
 public class FullScreenFragment extends Fragment {
+    FragmentFullscreenBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentFullscreenBinding.inflate(inflater);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
--- app/src/main/java/com/steg/tencrypt/ui/ResetPin.java
+++ app/src/main/java/com/steg/tencrypt/ui/ResetPin.java
@@ -1,6 +1,29 @@
 package com.steg.tencrypt.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.steg.tencrypt.databinding.FragmentResetBinding;
+
 public class ResetPin extends Fragment {
+    FragmentResetBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding= FragmentResetBinding.inflate(inflater);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
